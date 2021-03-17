32ef2dffaf0fc15b1a8038b2bc51beae743c75c3
Ssssanja <61282557+Ssssanja@users.noreply.github.com>
2021-03-17 23:41:58 +0300

    Show All Employees Using http Get-method

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
@@ -1,9 +1,13 @@
 package com.zaurtregulov.spring.rest.controller;
 
+import com.zaurtregulov.spring.rest.entity.Employee;
 import com.zaurtregulov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -11,7 +15,11 @@ public class MyRestController {
     @Autowired
     private EmployeeService employeeService;
 
-
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee>allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
 
 
 }
