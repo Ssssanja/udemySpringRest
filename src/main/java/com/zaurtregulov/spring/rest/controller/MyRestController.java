17c6f3fc52ea6d427b6bf6ef73b5d376dd345356
Ssssanja <61282557+Ssssanja@users.noreply.github.com>
2021-03-18 20:19:37 +0300

    Global Exception Handler added

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
@@ -32,19 +32,5 @@ public class MyRestController {
         return employee;
     }
 
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handelException(NoSuchEmployeeException exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handelException(Exception exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-    }
-
 
 }
